cb6f045cdfaa88426dbf53a524bb2aa444d99406
JEROMEBRANCHETTI <jeromebranchetti00@gmail.com>
2022-01-04 08:17:44 +0100

    movies e songs

--- src/main/java/org/generation/italy/controllers/IndexController.java
+++ src/main/java/org/generation/italy/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package org.generation.italy.controllers;
 
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,21 @@ public class IndexController {
 		
 		
 		return "index";
+	}
+	
+	@GetMapping("/movies")
+//	@ResponseBody
+	public String movies(Model model) {
+		model.addAttribute("movies",Arrays.asList("Sing 2","West Side Story","Supereroi","Chi ha incastrato Babbo Natale?\n" + 
+				"","Diabolik","House of Gucci","Spider-Man: No Way Home"));
+		return "movies";
+	}
+	
+	@GetMapping("/songs")
+//	@ResponseBody
+	public String songs(Model model) {
+		model.addAttribute("songs",Arrays.asList("Malibù ‑ Sangiovanni","Mi fai impazzire ‑ Blanco & Sfera Ebbasta","Mille ‑ Fedez & Achille Lauro & Orietta Berti","Stay - The Kid Laroi & Justin Bieber	","Zitti e buoni - Maneskin	","Musica leggerissima - Colapesce & Dimartino	","La canzone nostra - Mace & Blanco & Salmo	"));
+		return "songs";
 	}
 
 }
