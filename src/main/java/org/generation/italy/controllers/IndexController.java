18f5abcd9acd3446a3b2137f2b6e48442ef23588
JEROMEBRANCHETTI <jeromebranchetti00@gmail.com>
2022-01-10 20:01:11 +0100

    first commit

--- src/main/java/org/generation/italy/controllers/IndexController.java
+++ src/main/java/org/generation/italy/controllers/IndexController.java
@@ -26,14 +26,14 @@ public class IndexController {
 //	@ResponseBody
 	public String movies(Model model) {
 		model.addAttribute("movies",Arrays.asList("Sing 2","West Side Story","Supereroi","Chi ha incastrato Babbo Natale?\n" + 
-				"","Diabolik","House of Gucci","Spider-Man: No Way Home"));
+				"","Diabolik","House of Gucci","Spider-Man: No Way Home","Stay Close","The Witcher"));
 		return "movies";
 	}
 	
 	@GetMapping("/songs")
 //	@ResponseBody
 	public String songs(Model model) {
-		model.addAttribute("songs",Arrays.asList("Malibù ‑ Sangiovanni","Mi fai impazzire ‑ Blanco & Sfera Ebbasta","Mille ‑ Fedez & Achille Lauro & Orietta Berti","Stay - The Kid Laroi & Justin Bieber	","Zitti e buoni - Maneskin	","Musica leggerissima - Colapesce & Dimartino	","La canzone nostra - Mace & Blanco & Salmo	"));
+		model.addAttribute("songs",Arrays.asList("Malibù ‑ Sangiovanni","Mi fai impazzire ‑ Blanco & Sfera Ebbasta","Mille ‑ Fedez & Achille Lauro & Orietta Berti","Stay - The Kid Laroi & Justin Bieber	","Zitti e buoni - Maneskin	","Musica leggerissima - Colapesce & Dimartino	","La canzone nostra - Mace & Blanco & Salmo","Rain On Me - Lady Gaga & Ariana Grande","Adore You - Harry Styles" ));
 		return "songs";
 	}
 
